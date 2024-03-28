fc954510ad59db85e4dcdc3ca9d58e959af9f074
Tommi S. HAMK <72154152+TommiHAMK@users.noreply.github.com>
2024-03-28 15:00:00 +0200

    Added Products loop and 404

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+// Dummy database. IRL we would get the data from the MongoDB.
+const products = 
+[
+    {
+        name: 'Kia',
+        price: 25000
+    },
+    {
+        name: 'Volvo',
+        price: 30000
+    }
+]
+
+
 app.engine('handlebars',exphbs.engine({
     defaultLayout: 'main'
 }));
@@ -24,8 +38,21 @@ app.get('/contact', (req,res) => {
     res.render('contact');
 });
 
+app.get('/products', (req,res) => {
+    res.render('products',
+    {
+       title: "Products",
+       products: products
+    }
+    )
+});
+
 // Folder for static files like css, jpg...
 app.use(express.static('public'));
+
+app.use((req,res,next) => {
+    res.status(404).send("Sorry, could not find the content");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
